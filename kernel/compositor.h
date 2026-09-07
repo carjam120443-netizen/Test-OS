1acfbb63cf99cb1624b72d5c5c5b6caef36cad58
carjam120443-netizen <carjam120443@gmail.com>
2026-09-07 08:25:43 -0400

    Add movable and closable compositor windows

--- kernel/compositor.h
+++ kernel/compositor.h
@@ -8,6 +8,9 @@ struct window { uint32_t id, x, y, width, height, flags; };
 
 void compositor_init(void);
 int compositor_create_window(uint32_t x, uint32_t y, uint32_t width, uint32_t height);
+int compositor_move_window(uint32_t id, uint32_t x, uint32_t y);
+int compositor_resize_window(uint32_t id, uint32_t width, uint32_t height);
+int compositor_close_window(uint32_t id);
 const struct window *compositor_window(uint32_t id);
 uint32_t compositor_window_count(void);
 
