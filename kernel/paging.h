b81d6306d1f7ba1087906dfad67e2642a6215df0
carjam120443-netizen <carjam120443@gmail.com>
2026-09-07 08:14:25 -0400

    Expose per-process bootstrap address-space API

--- kernel/paging.h
+++ kernel/paging.h
@@ -1,4 +1,11 @@
 #ifndef TEST_OS_PAGING_H
 #define TEST_OS_PAGING_H
+
+#include <stdint.h>
+
 int paging_init(void);
+uint32_t paging_create_user_space(void);
+int paging_switch(uint32_t page_directory);
+uint32_t paging_current(void);
+
 #endif
